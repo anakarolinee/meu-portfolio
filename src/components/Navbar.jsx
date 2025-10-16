b8169f8bc5f2632a79dd8ddd959cd5e5a98d7ae8
Ana Karoline Araujo Da Silva <89096390+anakarolinee@users.noreply.github.com>
2025-10-16 13:32:56 -0300

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -54,7 +54,7 @@ const Navbar = () => {
               </li>
             </li>
           </ul>
-          <div className="relative md:hidden">
+          <div className="relative md:hidden" ref={menuRef}>
             <button
               onClick={() => setMenuOpen(!menuOpen)}
               className={`text-purple-800 focus:outline-none transition-transform ${
@@ -65,7 +65,6 @@ const Navbar = () => {
             </button>
             {menuOpen && (
               <ul
-                ref={menuRef} 
                 className="absolute right-0 mt-4 w-48 rounded-lg bg-white shadow-lg"
               >
                 <li className="border-b">
